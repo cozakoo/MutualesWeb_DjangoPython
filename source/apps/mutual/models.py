03ee8c7532e5c1ce59a9f8ec8b2071b5abaacd06
carloslucer <lucero.carlos.ch@gmail.com>
2024-01-05 11:02:46 -0300

    creacion de app mutual

--- source/apps/mutual/models.py
+++ source/apps/mutual/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Mutual(models.Model):
+
+    nombre = models.CharField(max_length=100)
+    cuit = models.CharField(max_length=30)
+    estado = models.BooleanField()
+    fecha_subida = models.DateField(auto_now_add=True, blank=True)
+    
+    def __str__(self):
+        return self.name
+    
+    
