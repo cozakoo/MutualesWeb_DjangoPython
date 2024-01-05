8915bc7436d7a4f7062d1b984895e1468f075106
carloslucer <lucero.carlos.ch@gmail.com>
2024-01-05 11:31:47 -0300

    new models

--- source/apps/mutual/models.py
+++ source/apps/mutual/models.py
@@ -8,8 +8,21 @@ class Mutual(models.Model):
     cuit = models.CharField(max_length=30)
     estado = models.BooleanField()
     fecha_subida = models.DateField(auto_now_add=True, blank=True)
+
     
     def __str__(self):
         return self.name
+
+class DeclaracionJurada(models.Model):
     
+    TIPO_DECLARACION = [
+        ('R', 'reclamo'),
+        ('P', 'prestamo'),
+    ]
+    mutual = models.ForeignKey(Mutual,on_delete = models.CASCADE)
+    tipo = models.CharField(max_length=1 , choices=TIPO_DECLARACION)
+    fecha_subida = models.DateField()
+    fecha_rectificacion = models.DateField()
+    periodo_declarado = models.DateField()
     
+
