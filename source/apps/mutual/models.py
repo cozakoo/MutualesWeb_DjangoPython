88ae4f47ecef48a74d1acc639c8e30c464e4a08b
Martin_arcos <martinarcosvargas2@gmail.com>
2024-02-22 11:48:37 -0300

    pdf generado

--- source/apps/mutual/models.py
+++ source/apps/mutual/models.py
@@ -99,11 +99,11 @@ class DeclaracionJurada(models.Model):
     mutual = models.ForeignKey(Mutual, on_delete=models.CASCADE)
     periodo = models.ForeignKey(Periodo, on_delete=models.CASCADE)
     detalles = models.ManyToManyField(DetalleDeclaracionJurada,related_name='detalles', blank=True, through = "DeclaracionJuradaDetalles")
-
     fecha_subida = models.DateField(null=True, blank=True)
+    codigo_acuse_recibo = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    
     fecha_creacion = models.DateField(auto_now_add=True) # fecha creación del borrador
     rectificativa = models.IntegerField(default=0)
-    codigo_acuse_recibo = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     es_leida = models.BooleanField(default=False)
     es_borrador = models.BooleanField(default=True)
     
