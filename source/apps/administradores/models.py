fdc1da32e740e55c833c4c7848dae03d43a9fef4
carloslucer <lucero.carlos.ch@gmail.com>
2024-03-06 10:56:30 -0300

    cambio nombre model administrador

--- source/apps/administradores/models.py
+++ source/apps/administradores/models.py
@@ -3,11 +3,11 @@ from apps.personas.models import Rol
 from utils.regularexpressions import alpha_validator
 
 # Create your models here.
-class Adminitrador(Rol):
+class Administrador(Rol):
     TIPO = 2
     nombre = models.CharField(max_length=50, validators=[alpha_validator])
     apellido = models.CharField(max_length=50, validators=[alpha_validator])
     def __str__(self):
         return f"{self.persona.nombre} {self.persona.apellido}"
 
-Rol.register(Adminitrador)
+Rol.register(Administrador)
